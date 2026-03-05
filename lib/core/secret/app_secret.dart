ad86163a6ce70fd1790a9da21b7d7c415ea2e016
Abdelrahman Elbendary <a.bendary@tidalsystems.net>
2026-03-05 13:49:16 +0200

    setup supabase

---
+++ lib/core/secret/app_secret.dart
@@ -0,0 +1,5 @@
+class AppSecret {
+  static const String supabaseURL = 'https://xokpyizyahykgdlakboa.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhva3B5aXp5YWh5a2dkbGFrYm9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MDg0NDMsImV4cCI6MjA4ODI4NDQ0M30.UoDmTpIRmaStul4aSS_guR4-Pdgjpl5OgUBSiLju94c';
+}
